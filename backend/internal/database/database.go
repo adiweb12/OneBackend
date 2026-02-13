939f446e6b18b6678b23f9aede88092bf4f080c8
adiweb12 <vsadith051@gmail.com>
2026-02-13 18:45:56 +0530

    Add files via upload

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,1 +1,45 @@
+package database
 
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"onechat/internal/models"
+)
+
+func InitDB(databaseURL string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	log.Println("Database connection established successfully")
+	return db, nil
+}
+
+func AutoMigrate(db *gorm.DB) error {
+	log.Println("Running database migrations...")
+	
+	err := db.AutoMigrate(
+		&models.User{},
+		&models.Chat{},
+		&models.Message{},
+		&models.Group{},
+		&models.GroupMember{},
+		&models.Event{},
+		&models.Media{},
+		&models.MessageStatus{},
+	)
+	
+	if err != nil {
+		return fmt.Errorf("failed to migrate database: %w", err)
+	}
+	
+	log.Println("Database migrations completed successfully")
+	return nil
+}
